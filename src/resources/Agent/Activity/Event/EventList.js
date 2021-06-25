39e562b02f0ba688689c0902bc9f5ba24fe8dc5d
srosset81 <srosset81@gmail.com>
2021-06-25 17:58:59 +0200

    Add calendar view

--- src/resources/Agent/Activity/Event/EventList.js
+++ src/resources/Agent/Activity/Event/EventList.js
@@ -1,29 +1,47 @@
 import React from 'react';
-import { DateField } from 'react-admin';
-import { Avatar } from '@material-ui/core';
-import { List, SimpleList } from '@semapps/archipelago-layout';
+import { CalendarList, DaysList } from '@semapps/date-components';
+import { MultiViewsList } from '@semapps/archipelago-layout';
+import frLocale from '@fullcalendar/core/locales/fr';
 import EventIcon from '@material-ui/icons/Event';
+import ListIcon from '@material-ui/icons/List';
 
 const EventList = props => (
-  <List title="Agenda partagé" sort={{ field: 'pair:startDate', order: 'DESC' }}{...props}>
-    <SimpleList
-      primaryText={record => record['pair:label']}
-      secondaryText={record => (
-        <>
-          Du&nbsp;
-          <DateField record={record} source="pair:startDate" showTime />
-          &nbsp;au&nbsp;
-          <DateField record={record} source="pair:endDate" showTime />
-        </>
-      )}
-      leftAvatar={() =>
-        <Avatar width="100%">
-          <EventIcon />
-        </Avatar>
+  <MultiViewsList
+    title="Agenda partagé"
+    views={{
+      calendar: {
+        label: 'Calendrier',
+        icon: EventIcon,
+        perPage: 1000,
+        pagination: false,
+        list: (
+          <CalendarList
+            locale={frLocale}
+            label="pair:label"
+            startDate="pair:startDate"
+            endDate="pair:endDate"
+            linkType="show"
+          />
+        )
+      },
+      list: {
+        label: 'Liste',
+        icon: ListIcon,
+        perPage: 1000,
+        pagination: false,
+        list: (
+          <DaysList
+            locale={frLocale}
+            label="pair:label"
+            startDate="pair:startDate"
+            endDate="pair:endDate"
+            linkType="show"
+          />
+        )
       }
-      linkType="show"
-    />
-  </List>
+    }}
+    {...props}
+  />
 );
 
 export default EventList;
